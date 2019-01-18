b06e134e96863f7946199a51c7425907f7979dbe
Michael Chung <michael.chung1994@gmail.com>
2019-01-18 00:44:43 +0000

    finished tutorial to allow user to login, signup and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
+  
+  # root to: 'gif#cool'
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
@@ -17,6 +19,14 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:index, :new, :create]
   end
+
+    # these routes are for showing users a login form, logging them in, and logging them out.
+    get '/login' => 'sessions#new'
+    post '/login' => 'sessions#create'
+    get '/logout' => 'sessions#destroy'
+  
+    get '/signup' => 'users#new'
+    post '/users' => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -73,3 +83,19 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 end
+
+# config/routes.rb
+
+# GifVault::Application.routes.draw do
+
+#   root to: 'gif#cool'
+
+#   # these routes are for showing users a login form, logging them in, and logging them out.
+#   get '/login' => 'sessions#new'
+#   post '/login' => 'sessions#create'
+#   get '/logout' => 'sessions#destroy'
+
+#   get '/signup' => 'users#new'
+#   post '/users' => 'users#create'
+  
+# end
